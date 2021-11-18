110343f4a8a932af14d774686cd6f7222a6841d5
kunpeisatou <kunpei0411@gmail.com>
2021-11-18 11:29:22 +0000

    ジャンル登録機能作成

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,50 @@
 class Admin::GenresController < ApplicationController
+  
+  def create
+    @genre = Genre.new(genre_params)
+    
+    if @genre.save
+      flash[:notice] = 'You have created genre successfully.'
+      redirect_to admin_genres_path
+    else
+      
+      
+      render :index
+    end
+
+  end
+
+  def index
+    @genres = Genre.all
+    #@genres = Genre.page(params[:page]).reverse_order
+    @genre = Genre.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+       flash[:notice] = 'You have updated genre successfully.'
+       redirect_to admin_genres_path
+    else
+       render :edit
+    end
+    
+  end
+
+  # 投稿データのストロングパラメータ
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+  
 end
