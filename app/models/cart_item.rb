29de818b3f6e7c018c2b0aad2199f018051cf969
atsuki04 <cavu04hmp@gmail.com>
2021-11-19 06:01:35 +0000

    Add add_cart_item function

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,10 @@
 class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
+  
+  private
+def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+end
+
 end
