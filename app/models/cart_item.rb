8fb33c34fb8c0b0083d717ecc8f0e0af7e3a0fc2
atsuki04 <cavu04hmp@gmail.com>
2021-11-20 04:48:49 +0000

    Add cart index

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,10 @@ class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
   
+  def subtotal
+    item.with_tax_price * amount
+  end  
+  
   private
 def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
