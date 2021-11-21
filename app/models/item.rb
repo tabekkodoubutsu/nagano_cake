6ca3f1ace399f2cba2fe3408015fa527ab62954b
atsuki04 <cavu04hmp@gmail.com>
2021-11-21 02:40:03 +0000

    Finish cart functions2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,10 @@
 class Item < ApplicationRecord
   has_many :cart_items
   attachment :image
-  
+
   def with_tax_price
     (price * 1.1).floor
   end
   # 消費税を求めるメソッド
- 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :show]
     resources :cart_items, only: [:index, :create, :update, :destroy]
     delete "/cart_items" => "cart_items#all_destroy"
+    get 'cart_items/destroy_all' => 'cart_items#destroy_all'
     resources :orders, only: [:new, :create, :index, :show]
     post "/orders/confirm" => "orders#confirm"
     get "/orders/complete" => "orders#complete"
