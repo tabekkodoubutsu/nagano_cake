5aeb5e2d956a1b3232f62dd3976bb8f1d8480197
kunpeisatou <kunpei0411@gmail.com>
2021-11-22 09:13:41 +0000

    顧客詳細編集

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.i18n.default_locale = :ja
-config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -14,5 +11,7 @@ module NaganoCake
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
   end
 end
