a845e8e1bc10735657bfc1d9d1147f6883ba1eb6
tstm2110 <ts2021ik06@gmail.com>
2021-11-23 05:32:17 +0000

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -21,15 +21,6 @@ ActiveRecord::Schema.define(version: 2021_11_22_084006) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "addresses", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "postal_code"
-    t.string "address"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
