4ac3c46bab00524cd4d0b20bb4b2f656b5c85ed8
atsuki04 <cavu04hmp@gmail.com>
2021-11-23 09:12:10 +0000

    Add orderコントローラーにconfig機能

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,11 +3,34 @@ class Public::OrdersController < ApplicationController
         @order = Order.new
     end
 
-
+    def confirm
+        @order = Order.new(order_params)
+        if params[:order][:select_address] == "0"
+            @order.postal_code = current_customer.postal_code
+            @order.address = current_customer.address
+            @order.name = current_customer.last_name + current_customer.first_name
+            
+        elsif params[:order][:select_address] == "1"
+            @address = Address.find(params[:order][:address_id])
+            @order.postal_code = @address.postal_code
+            @order.address = @address.address
+            @order.name = @address.name
+        
+        elsif params[:order][:select_address] == "2"
+            address_new = current_customer.address.new(address_params)
+            if address_new.save
+            else
+              render :new
+            end
+        end
+        @cart_items = current_customer.cart_items.all
+        @total = 0
+        @total_payment = SHIPPING_COST + @total
+    end    
 
     private
     def order_params
-      params.require(:order).permit(:pyament_method, :postal_code, :address, :name, :shipping_cost, :total_payment)
+      params.require(:order).permit(:payment_method, :postal_code, :address, :name, :shipping_cost, :total_payment)
     end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,9 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :order_details
   
+  def subtotal
+    item.with_tax_price * amount
+  end
+
   enum payment_method: { credit_card: 0, transfer: 1 }
 end
