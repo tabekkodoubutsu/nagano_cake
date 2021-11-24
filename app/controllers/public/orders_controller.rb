6b6f0e398fa8f4dfc026be8d8b39a71fd9fdef9e
atsuki04 <cavu04hmp@gmail.com>
2021-11-24 02:14:33 +0000

    Add order_create action

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -13,13 +13,13 @@ class Public::OrdersController < ApplicationController
             @order.postal_code = current_customer.postal_code
             @order.address = current_customer.address
             @order.name = current_customer.last_name + current_customer.first_name
-            
+
         elsif params[:order][:select_address] == "1"
             @address = Address.find(params[:order][:address_id])
             @order.postal_code = @address.postal_code
             @order.address = @address.address
             @order.name = @address.name
-        
+
         elsif params[:order][:select_address] == "2"
             address_new = current_customer.address.new(address_params)
             if address_new.save
@@ -27,7 +27,29 @@ class Public::OrdersController < ApplicationController
               render :new
             end
         end
-    end    
+    end
+
+    def create
+        cart_items = current_customer.cart_items.all
+        @order = current_customer.orders.new(order_params)
+        if @order.save
+          cart_items.each do |cart_item|
+              order_detail = OrderDetail.new
+              order_detail.item_id = cart_item.item_id
+              order_detail.order_id = @order.id
+              order_detail.amount = cart_item.amount
+              order_detail.price = cart_item.item.price
+              order_detail.save
+          end
+          current_customer.cart_items.destroy_all
+          redirect_to orders_thanks_path
+        else
+            render :new
+        end
+    end
+
+    def thanks
+    end
 
     private
     def order_params
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
-  has_many :order_details
-  
-  def subtotal
-    item.with_tax_price * amount
-  end
+  has_many :order_details, dependent: :destroy
 
   enum payment_method: { credit_card: 0, transfer: 1 }
 end
