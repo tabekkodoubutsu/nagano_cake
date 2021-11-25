44ede5726cfaa0ffd15b2c337526981426461107
tstm2110 <ts2021ik06@gmail.com>
2021-11-25 12:59:06 +0000

    add 検窓

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,11 +2,12 @@ class Public::ItemsController < ApplicationController
 
     def index
         @items=Item.all
-        
+        @genres=Genre.all
     end
 
     def show
       @cart_item = CartItem.new
       @item = Item.find(params[:id])
+      @genres=Genre.all
     end
 end
